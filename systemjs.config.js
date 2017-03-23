437fca42634fa8acddbbf071eefdab0593f4a702
Matej Minarik <mato.minarik@azet.sk>
2017-03-23 14:12:39 +0100

    SystemJS config adjustments

--- systemjs.config.js
+++ systemjs.config.js
@@ -6,12 +6,12 @@
   System.config({
     paths: {
       // paths serve as alias
-      'npm:': 'node_modules/'
+      'npm:': '/node_modules/'
     },
     // map tells the System loader where to look for things
     map: {
       // our app is within the app folder
-      'app': 'app',
+      'app': '/app',
 
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
@@ -22,6 +22,7 @@
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      '@angular/upgrade/static': 'npm:@angular/upgrade/bundles/upgrade-static.umd.js',
 
       // other libraries
       'rxjs':                      'npm:rxjs',
