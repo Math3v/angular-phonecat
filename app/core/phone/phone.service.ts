d4aac96399ad52974b4ac307789c79d4a5d2a5ae
Matej Minarik <mato.minarik@azet.sk>
2017-06-13 13:35:56 +0200

    PhoneService refactored to class

--- app/core/phone/phone.service.ts
+++ app/core/phone/phone.service.ts
@@ -1,15 +1,26 @@
-'use strict';
+class PhoneService {
+
+  resource: angular.resource.IResourceService;
+  
+  static $inject = ['$resource'];
+
+  constructor($resource: angular.resource.IResourceService) {
+    this.resource = $resource;
+  }
+
+  get() {
+    return this.resource('phones/:phoneId.json', {}, {
+      query: {
+        method: 'GET',
+        params: {phoneId: 'phones'},
+        isArray: true
+      }
+    });
+  }
+}
 
 angular.
   module('core.phone').
-  factory('Phone', ['$resource',
-    function($resource: angular.resource.IResourceService) {
-      return $resource('phones/:phoneId.json', {}, {
-        query: {
-          method: 'GET',
-          params: {phoneId: 'phones'},
-          isArray: true
-        }
-      });
-    }
-  ]);
+  factory('Phone', ['$resource', function($resource) {
+    return new PhoneService($resource).get();
+  }]);
