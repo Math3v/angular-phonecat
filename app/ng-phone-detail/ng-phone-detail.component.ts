8cd8f36ae7450d91fb85160000bed25862129b4d
Matej Minarik <mato.minarik@azet.sk>
2017-06-13 14:41:41 +0200

    AngularJS Phone service used from Angular component

--- app/ng-phone-detail/ng-phone-detail.component.ts
+++ app/ng-phone-detail/ng-phone-detail.component.ts
@@ -2,19 +2,30 @@ import { Component, OnInit } from '@angular/core';
 declare const angular: any;
 import { downgradeComponent } from '@angular/upgrade/static';
 
+import { Phone } from '../core/phone/phone.service';
+
 @Component({
   selector: 'ng-phone-detail',
   template:  `
     <h1>Hello from phone detail Angular</h1>
+    <p>{{ phone | json }}</p>
   `
 })
 export class PhoneDetailComponent implements OnInit {
   
+  phone: any;
+
   constructor(
+    private phoneService: Phone
   ) {}
 
   ngOnInit() {
-    console.log(  );
+    this.phoneService.getPhoneResource().get({
+      phoneId: 'motorola-xoom'
+      }, (phone: any) => {
+        this.phone = phone;
+      }
+    );
   }
 }
 
