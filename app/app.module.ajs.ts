a17e0291c2b80e58dbf226fe4cc942cbb2834791
Matej Minarik <mato.minarik@azet.sk>
2017-06-15 12:03:11 +0200

    MainController refactored to separate file

--- app/app.module.ajs.ts
+++ app/app.module.ajs.ts
@@ -9,24 +9,3 @@ angular.module('phonecatApp', [
   'phoneList',
   'ngjsDropdownSelectModule'
 ]);
-
-angular.module('phonecatApp')
-  .controller('MainController', ['$scope', '$timeout', function($scope, $timeout) {
-    $scope.test = function() {
-      console.log('MainController.test ', arguments);
-    }
-
-    $scope.items = [
-      { id: 1, name: 'Account' },
-      { id: 2, name: 'Logout' }
-    ];
-
-    $timeout(function() {
-      let newItem = {
-        id: 3,
-        name: 'Dynamic item'
-      };
-
-      $scope.items = [...$scope.items, newItem];
-    }, 2500);
-  }]);
---
+++ app/main.controller.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('phonecatApp')
+  .controller('MainController', ['$scope', '$timeout', function($scope, $timeout) {
+    $scope.test = function() {
+      console.log('MainController.test ', arguments);
+    }
+
+    $scope.items = [
+      { id: 1, name: 'Account' },
+      { id: 2, name: 'Logout' }
+    ];
+
+    $timeout(function() {
+      let newItem = {
+        id: 3,
+        name: 'Dynamic item'
+      };
+
+      $scope.items = [...$scope.items, newItem];
+    }, 2500);
+  }]);
